c390eb541e3c841528f9815ef3a4e1329f033580
Pandu Persada Tanjung <83505767+pandutanjung@users.noreply.github.com>
2023-11-30 11:41:41 +0700

    bug in UI

--- formcatatan.php
+++ formcatatan.php
@@ -1,57 +1,94 @@
-<?php 
-    include("config.php");
+<?php
+    session_start();
+    
+    if (!isset($_SESSION['username'])) {
+        header("Location: login.php");
+        exit;
+    }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Iconscout Link For Icons -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,600,0,0" />
+    <link rel="stylesheet" href="https://unicons.iconscout.com/realease/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="form_catatan.css">
+    
     <title>Catatan | Cakepp</title>
 </head>
 <body>
-    <header>
-        <h3>Tambah Catatan Kelasmu</h3>
-    </header>
+    <div class="flex-container">
+        <div class="header">
+        </div>
+        <div class="flex-box">
+            <h2>My Notes</h2> 
+        </div>
+        <div class="formtop">
+            <form action="prosestambahcatatan.php" method="POST" id="form">
+                <div class="input-field">
+                    <div class="leftside">
+                        <div class="title-left">
+                            <label for="title-left">Title</label>
+                            <input type="text" id="title-left" name="judul_catatan">
+                        </div>
+                        <div class="topic">
+                            <label for="topic">Course</label>
+                            <input type="text" id="topic" name="nama_matkul">
+                        </div>
+                        <div class="topic">
+                            <label for="task">Chapter</label>
+                            <input type="text" id="task" name="nama_bab">
+                        </div>
+                    </div>
+                    <div class="rightside">
+                        <div class="subject">
+                            <label for="subject">Week</label>
+                            <input list="pertemuan" name="pertemuan_ke">
+                            <datalist id="pertemuan">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                            </datalist>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-field-main">
+                    <div class="notes">
+                        <h3>Notes</h3>
+                        <textarea type="text" id="notes" rows="25" placeholder="Write your note here!!" name="isi_catatan"></textarea>
+                    </div>
+                    <div class="link-list">
+                        <h3>Link</h3>
+                        <textarea type="text" id="link-list" rows="25" placeholder="Type the links or sources!!" name="kolom_link"></textarea>
+                    </div>
+                </div>
 
-    <form action="prosestambahcatatan.php" method="POST">
-        <fieldset>
-            <p>
-                <label for="judul_catatan">Judul Catatan: </label>
-                <input type="text" name="judul_catatan" placeholder="judul catatan" />
-            </p>
-            <p>
-                <label for="pertemuan_id">Pertemuan: </label>
-                <input list="pertemuan" name="pertemuan_id">
-                <datalist id="pertemuan">
-                    <?=
-                        $pertemuan = pg_query("SELECT id FROM pertemuan");
-                        while (1) {
-                           echo "<option value='$pertemuan'>";
-                        }
-                    ?>
-                </datalist>
-            </p>
-            <p>
-                <label for="nama_matkul">Mata Kuliah: </label>
-                <input type="text" name="nama_matkul" placeholder="mata kuliah" />
-            </p>
-            <p>
-                <label for="nama_bab">Bab: </label>
-                <input type="text" name="nama_bab" placeholder="bab" />
-            </p>
-            <p>
-                <label for="isi_catatan">Isi Catatan: </label>
-                <textarea name="isi_catatan"></textarea>
-		    </p>
-            <p>
-                <label for="kolom_link">Link: </label>
-                <input type="text" name="kolom_link" placeholder="link" />
-            </p>
-            <p>
-			    <input type="submit" value="Tambahkan" name="Tambah" />
-		    </p>
-        </fieldset>
-    </form>
+                <button type="submit" name="tambah">Save</button>
+                <a href="daftarcatatan.php" id="back">Back</a>
+            </form>
+            
+        </div>
+
+        
+
+    </div>
+    
+    <script src="notes.js"></script>
+
 </body>
 </html>
--- formtugas.php
+++ formtugas.php
@@ -1,61 +1,90 @@
-<?php 
-    include("config.php");
+<?php
+    session_start();
+    
+    if (!isset($_SESSION['username'])) {
+        header("Location: login.php");
+        exit;
+    }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas | Cakepp</title>
+    <title>Assigment</title>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/realease/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="form_tugas.css">
+    
 </head>
 <body>
-    <header>
-        <h3>Tambah Tugas Kelasmu</h3>
-    </header>
+    <div class="flex-container">
+        <div class="header">
+        </div>
+        <div class="flex-box">
+            <h2>Tugas</h2> 
+        </div>
+        
+        <div class="new-assignment">
+            <div class="assignment-box">
+                <div class="input-field">
+                    <div class="form-assignment">
+                        <form action="prosestambahtugas.php" method="POST">
+                            <div class="title">
+                                <label for="title">Title </label>
+                                <input type="text" id="title" name="judul_tugas" required >
+                            </div>
+                            <div class="week">
+                                <label for="week">Week </label>
+                                <input list="pertemuan" name="pertemuan_ke">
+                                <datalist id="pertemuan">
+                                    <option value="1">
+                                    <option value="2">
+                                    <option value="3">
+                                    <option value="4">
+                                    <option value="5">
+                                    <option value="6">
+                                    <option value="7">
+                                    <option value="8">
+                                    <option value="9">
+                                    <option value="10">
+                                    <option value="11">
+                                    <option value="12">
+                                    <option value="13">
+                                    <option value="14">
+                                </datalist>
+                            </div>
+                            <div class="course">
+                                <label for="course">Course </label>
+                                <input type="text" id="course" name="nama_matkul">
+                            </div>
+                            <div class="chapter">
+                                <label for="chapter">Chapter </label>
+                                <input type="text" id="chapter" name="nama_bab">
+                            </div>
+                            <div class="deadline">
+                                <label for="chapter">Deadline </label>
+                                <input type="date" id="deadline" name="deadline_tugas">
+                            <div class="task-link">
+                                <label for="link">Link </label>
+                                <input type="text" id="task-link" name="kolom_link">
+                            </div>
+                            <div class="desc">
+                                <label for="link">Description </label>
+                                <textarea name="desc" cols="30" rows="10" name="deskripsi_tugas"></textarea>
+                            </div>
+                            <div class="button-task">
+                                
+                            </div>
+                            <button type="submit" name="tambah">Save</button>
+                            <a href="daftartugas.php" id="back">Back</a>
+                        </form>
+                        
+                    </div>
+                </div>               
+            </div>
+    </div>
+    <script src="assignment.js"></script>
 
-    <form action="prosestambahtugas.php" method="POST">
-        <fieldset>
-            <p>
-                <label for="judul_tugas">Judul Tugas: </label>
-                <input type="text" name="judul_tugas" placeholder="judul tugas" />
-            </p>
-            <p>
-                <label for="pertemuan_id">Pertemuan: </label>
-                <input list="pertemuan" name="pertemuan_id">
-                <datalist id="pertemuan">
-                    <?php
-                        $pertemuan = pg_query("SELECT id FROM pertemuan");
-                        while ($row = pg_fetch_assoc($pertemuan)) {
-                           echo "<option value='" . $row['id'] . "'>";
-                        }
-                    ?>
-                </datalist>
-            </p>
-            <p>
-                <label for="nama_matkul">Mata Kuliah: </label>
-                <input type="text" name="nama_matkul" placeholder="mata kuliah" />
-            </p>
-            <p>
-                <label for="nama_bab">Bab: </label>
-                <input type="text" name="nama_bab" placeholder="bab" />
-            </p>
-            <p>
-                <label for="isi_tugas">Isi Tugas: </label>
-                <textarea name="isi_tugas"></textarea>
-            </p>
-            <p>
-                <label for="deadline">Deadline: </label>
-                <input type="date" name="deadline" />
-            </p>
-            <p>
-                <label for="kolom_link">Link Tempat Pengumpulan: </label>
-                <input type="text" name="kolom_link" placeholder="link" />
-            </p>
-            <p>
-                <input type="submit" value="Tambahkan" name="Tambah" />
-            </p>
-        </fieldset>
-    </form>
 </body>
 </html>
