683bd79031898be81dcdf18a1365231e7c8f8b8b
Pandu Persada Tanjung <83505767+pandutanjung@users.noreply.github.com>
2023-12-02 07:51:01 +0700

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $db = pg_connect('host=localhost dbname=cakepp user=postgres password=postgres');
-    if (!$db) {
-        die("Failed to connected database: " . pg_connect_error());
-    }
-?>
